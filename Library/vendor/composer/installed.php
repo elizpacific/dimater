46a00b67199a6b2ad116ba01181fb73affd22e0e
elizpacific <elizpacific@gmail.com>
2022-11-29 13:25:18 +0200

    changes

---
+++ Library/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c09d4250d3041ab454b7af6ad3a2ba644b92663b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c09d4250d3041ab454b7af6ad3a2ba644b92663b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'gingerpayments/ginger-php' => array(
+            'pretty_version' => '2.2.0',
+            'version' => '2.2.0.0',
+            'reference' => '38c32c48a4ce0e5344bbf591419803d0135e08d4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../gingerpayments/ginger-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
